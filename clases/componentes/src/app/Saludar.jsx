433e843ef1dd6619ae5c6257c9b1796690a8339c
DanCoVog <legendaryopspvp@gmail.com>
2025-08-27 08:45:12 -0500

    saludar destructurado

--- clases/componentes/src/app/Saludar.jsx
+++ clases/componentes/src/app/Saludar.jsx
@@ -9,8 +9,18 @@
 
 
 // creamos un componente
-export default function Saludar(props) {
+//export default function Saludar(props) {
+//  return (
+//    <p className="text-red-600 font-bold text-6xl hover:text-yellow-500">Hola soy un componente {props.nombre}, tienes {props.años} años</p>
+//  ) 
+//
+//}
+
+
+//creamo un componente aplicando destrucuturacion 
+
+export default function Saludar({ name, edad }) {
   return (
-    <p className="text-red-600 font-bold text-6xl hover:text-yellow-500">Hola soy un componente {props.nombre}, tienes {props.años} años</p>
-  ) 
+    <p className="text-red-600 font-bold text-6xl hover:text-yellow-500">Hola soy un componente {name}, tienes {edad} años</p>
+  )
 }
