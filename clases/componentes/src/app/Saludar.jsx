4eeb7ae5ceefafe252e7b7603e09ccfae61e78d2
DanCoVog <legendaryopspvp@gmail.com>
2025-08-27 08:34:01 -0500

    props

--- clases/componentes/src/app/Saludar.jsx
+++ clases/componentes/src/app/Saludar.jsx
@@ -1,6 +1,16 @@
+//props = {
+//  'name': 'juan',
+//  'id' : 12
+//}
+
+
+
+
+
+
 // creamos un componente
-export default function Saludar() {
+export default function Saludar(props) {
   return (
-    <p className="text-red-600 font-bold text-6xl hover:text-yellow-500">Hola soy un componente</p>
+    <p className="text-red-600 font-bold text-6xl hover:text-yellow-500">Hola soy un componente {props.nombre}</p>
   ) 
 }
