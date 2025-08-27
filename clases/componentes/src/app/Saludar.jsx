44fe4060337c091e233a4e319b92f690c9679579
DanCoVog <legendaryopspvp@gmail.com>
2025-08-27 08:27:50 -0500

    saludar

---
+++ clases/componentes/src/app/Saludar.jsx
@@ -0,0 +1,6 @@
+// creamos un componente
+export default function Saludar() {
+  return (
+    <p className="text-red-600 font-bold text-6xl hover:text-yellow-500">Hola soy un componente</p>
+  ) 
+}
